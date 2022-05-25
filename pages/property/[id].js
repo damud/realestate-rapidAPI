977e24722c4c872373d91de500a3b789fe0ec6c1
Daniel Mudlaff <dmudlaff89@gmail.com>
2022-05-25 09:36:23 +0200

    Create ImageScrollbar.jsx component & add it to details property page

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -3,6 +3,7 @@ import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
+import ImageScrollbar from '../../components/ImageScrollbar'
 
 import {baseUrl, fetchAPI } from "../../utils/fetchAPI";
 
