a8a9838ab84d243967e1298a6ac917b34f73be69
Daniel Mudlaff <dmudlaff89@gmail.com>
2022-05-28 19:09:15 +0200

    Add nice animated loader NProgress bar

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -12,35 +12,31 @@ const PropertyDetails = ({
     price,
     rentFrequency,
     rooms,
-    area,
     title,
     baths,
+    area,
     agency,
     isVerified,
     description,
     type,
     purpose,
     furnishingStatus,
-    photos,
     amenities,
+    photos,
   },
 }) => (
   <Box maxWidth="1000px" margin="auto" p="4">
     {photos && <ImageScrollbar data={photos} />}
     <Box w="full" p="6">
-      <Flex padding="2" alignItems="center" justifyContent="space-between">
-        <Flex alignItems="center">
-          <Box paddingRight="3" color="green.400">
-            {isVerified && <GoVerified />}
-          </Box>
-          <Text fontWeight="bold" fontSize="lg">
-            AED {millify(price)}
-            {rentFrequency && `/${rentFrequency}`}
-          </Text>
-        </Flex>
-        <Box>
-          <Avatar size="sm" src={agency?.logo?.url} />
+      <Flex paddingTop="2" alignItems="center">
+        <Box paddingRight="3" color="green.400">
+          {isVerified && <GoVerified />}
         </Box>
+        <Text fontWeight="bold" fontSize="lg">
+          AED {price} {rentFrequency && `/${rentFrequency}`}
+        </Text>
+        <Spacer />
+        <Avatar size="sm" src={agency?.logo?.url}></Avatar>
       </Flex>
       <Flex
         alignItems="center"
@@ -49,22 +45,44 @@ const PropertyDetails = ({
         w="250px"
         color="blue.400"
       >
-        {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft{" "}
-        <BsGridFill />
+        {rooms}
+        <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
       </Flex>
-      <Box marginTop="2">
-        <Text fontSize="lg" marginBottom="2" fontWeight="bold">
-          {title}
-        </Text>
-        <Text lineHeight="2" color="gray.600">
-          {description}
-        </Text>
-      </Box>
+    </Box>
+    <Box marginTop="2">
+      <Text fontSize="lg" marginBottom="2" fontWeight="bold">
+        {title}
+      </Text>
+      <Text lineHeight="2" color="gray.600">
+        {description}
+      </Text>
+    </Box>
+    <Flex
+      flexWrap="wrap"
+      textTransform="uppercase"
+      justifyContent="space-between"
+    >
       <Flex
-        flexWrap="wrap"
-        textTransform="uppercase"
         justifyContent="space-between"
+        w="400px"
+        borderBottom="1px"
+        borderColor="gray.100"
+        p="3"
       >
+        <Text>Type</Text>
+        <Text fontWeight="bold">{type}</Text>
+      </Flex>
+      <Flex
+        justifyContent="space-between"
+        w="400px"
+        borderBottom="1px"
+        borderColor="gray.100"
+        p="3"
+      >
+        <Text>Purpose</Text>
+        <Text fontWeight="bold">{purpose}</Text>
+      </Flex>
+      {furnishingStatus && (
         <Flex
           justifyContent="space-between"
           w="400px"
@@ -72,57 +90,35 @@ const PropertyDetails = ({
           borderColor="gray.100"
           p="3"
         >
-          <Text>Type</Text>
-          <Text fontWeight="bold">{type}</Text>
+          <Text>Furnishing Status</Text>
+          <Text fontWeight="bold">{furnishingStatus}</Text>
         </Flex>
-        <Flex
-          justifyContent="space-between"
-          w="400px"
-          borderBottom="1px"
-          borderColor="gray.100"
-          p="3"
-        >
-          <Text>Purpose</Text>
-          <Text fontWeight="bold">{purpose}</Text>
-        </Flex>
-        {furnishingStatus && (
-          <Flex
-            justifyContent="space-between"
-            w="400px"
-            borderBottom="1px"
-            borderColor="gray.100"
-            p="3"
-          >
-            <Text>Furnishing Status</Text>
-            <Text fontWeight="bold">{furnishingStatus}</Text>
-          </Flex>
+      )}
+    </Flex>
+    <Box>
+      {amenities.length && (
+        <Text fontSize="2xl" fontWeight="black" marginTop="5">
+          Facilites:
+        </Text>
+      )}
+      <Flex flexWrap="wrap">
+        {amenities?.map((item) =>
+          item?.amenities?.map((amenity) => (
+            <Text
+              key={amenity.text}
+              fontWeight="bold"
+              color="blue.400"
+              fontSize="l"
+              p="2"
+              bg="gray.200"
+              m="1"
+              borderRadius="5"
+            >
+              {amenity.text}
+            </Text>
+          ))
         )}
       </Flex>
-      <Box>
-        {amenities.lenght && (
-          <Text fontSize="2xl" fontWeight="black" marginTop="5">
-            Amenities
-          </Text>
-        )}
-        <Flex flexWrap="wrap">
-          {amenities.map((item) =>
-            item.amenities.map((amenity) => (
-              <Text
-                key={amenity.text}
-                fontWeight="bold"
-                color="blue.400"
-                fontSize="1"
-                p="2"
-                bg="gray.200"
-                m="1"
-                borderRadius="5"
-              >
-                {amenity.text}
-              </Text>
-            ))
-          )}
-        </Flex>
-      </Box>
     </Box>
   </Box>
 );
